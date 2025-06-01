ab068987449abf21c75475f7875ddedbbafffc26
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-06-01 18:07:12 +0530

    Setup tailwindcss and index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#2563eb',
+          600: '#1d4ed8',
+          700: '#1e40af',
+        },
+        secondary: {
+          500: '#8b5cf6'
+        },
+      }
+    },
   },
   plugins: [],
 }
